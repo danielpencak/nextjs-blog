f9a8979abb98be167c69f4e47ceab3b5877308a9
Daniel Pencak <danielpencak@gmail.com>
2023-10-09 19:39:10 -0500

    Update page content; add styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import Head from 'next/head';
-import Script from 'next/script';
+// import Script from 'next/script';
+
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -17,6 +19,6 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   )
 }
