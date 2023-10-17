a40fdcfca204b66b5954bc95aaf6cabf0987526b
Daniel Pencak <danielpencak@gmail.com>
2023-10-17 13:34:22 -0500

    Add TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export async function getStaticProps() {
 }
 
 
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData }: { allPostsData: { date: string; title: String;  id: string }}) {
   return (
     <Layout home>
       <Head>
